a563149e7e038e5878df04206684d9c5b9fec988
s1m0nSaW <simonsaw777@gmail.com>
2024-01-29 11:53:37 +0900

    change err.message to err in client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,11 +23,7 @@ const socket = io("https://ochem.ru/ws/", {
 
 socket.on("connect_error", (err) => {
   // the reason of the error, for example "xhr poll error"
-  console.log(err.message);
-  // some additional description, for example the status code of the initial HTTP response
-  console.log(err.description);
-  // some additional context, for example the XMLHttpRequest object
-  console.log(err.context);
+  console.log(err);
 });
 
 function App() {
