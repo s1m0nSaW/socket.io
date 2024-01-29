209acd6c902a2bcef8a4df62d809182c89d853f0
s1m0nSaW <simonsaw777@gmail.com>
2024-01-29 21:39:30 +0900

    change io.engine.on(connection_error)

--- backend/index.js
+++ backend/index.js
@@ -45,10 +45,7 @@ instrument(io, {
 });
 
 io.engine.on("connection_error", (err) => {
-    console.log(err.req);      // the request object
-    console.log(err.code);     // the error code, for example 1
     console.log(err.message);  // the error message, for example "Session ID unknown"
-    console.log(err.context);  // some additional error context
 });
 
 io.on("connection", (socket) => {
