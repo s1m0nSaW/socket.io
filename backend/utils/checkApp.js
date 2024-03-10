bc83328c9ce926b783976380b40af924564e735f
s1m0nSaW <simonsaw777@gmail.com>
2024-03-10 15:00:27 +0900

    with crypto in check app

--- backend/utils/checkApp.js
+++ backend/utils/checkApp.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import forge from 'node-forge';
+import crypto from 'crypto'
 
 dotenv.config()
 
@@ -7,33 +7,33 @@ const skey = process.env.SKEY
 
 export default (req, res, next) => {
     
-    var client_secret = skey;
-    var sign_param = req.body.appKey;
+    const clientSecret = skey;
+    const signParam = req.body.appKey;
 
     const hashParams = {
-        ts: req.body.ts, // Время создания подписи
-        request_id: req.body.request_id, // Присваиваем значение параметра payload
-        user_id: req.body.user_id, // Добавляем идентификатор пользователя
-        app_id: 51864614 // Добавляем идентификатор приложения
+    ts: req.body.ts, // Время создания подписи
+    request_id: req.body.request_id, // Присваиваем значение параметра payload
+    user_id: req.body.user_id, // Добавляем идентификатор пользователя
+    app_id: 51864614 // Добавляем идентификатор приложения
     };
 
     // Сортируем список по ключам
-    var hash_params = Object.keys(hash_params).sort().reduce((acc, key) => {
-        acc[key] = hash_params[key];
-        return acc;
-        }, {});
+    Object.keys(hashParams).sort().forEach(key => {
+    hashParams[key] = hashParams[key];
+    });
 
     // Формируем строку для вычисления подписи
-    var sign_params_query = Object.keys(hash_params).map(key => `${key}=${hash_params[key]}`).join('&');
+    let signParamsQuery = Object.keys(hashParams).map(key => `${key}=${hashParams[key]}`).join('&');
 
     // Формируем подпись, используя защищённый ключ приложения (метод HMAC)
-    var sign = forge.util.encode64(forge.md.hmacSha256.create().update(sign_params_query, 'utf8', client_secret).digest().getBytes());
+    const sign = crypto.createHmac('sha256', clientSecret).update(signParamsQuery).digest('base64');
 
     // Сравниваем полученную строку со значением из VKWebAppCreateHash
-    var status = sign === sign_param;
+    const status = sign === signParam;
 
-    console.log('мой ключ:', sign);
-    console.log('ключ vk:', sign);
+    console.log((status ? 'ok' : 'fail'));
+    console.log('my key:', sign);
+    console.log('my key:', signParam);
     if(status){
         next()
     } else {
